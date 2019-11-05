2b5185f8f83b89fda8087250d6c1fb4f78785b18
Dmitrij Koniajev <dimchansky@gmail.com>
2019-11-06 00:11:57 +0200

    defining GeoCoord/GeoBoundary/GeoPolygon

--- h3.go
+++ h3.go
@@ -1,8 +1,27 @@
 package h3
 
-// Index fits within a 64-bit unsigned integer
+// Index fits within a 64-bit unsigned integer.
 type Index = uint64
 
 // MaxCellBndryVerts is a maximum number of cell boundary vertices; worst case is pentagon:
 // 5 original verts + 5 edge crossings
 const MaxCellBndryVerts = 10
+
+// GeoCoord is a struct for geographic coordinates, holds latitude/longitude in radians.
+type GeoCoord struct {
+	Lat float64 // latitude in radians
+	Lon float64 // longitude in radians
+}
+
+// GeoBoundary is a slice of `GeoCoord`.  Note, `len(GeoBoundary)` will never
+// exceed `MaxCellBndryVerts`. Vertices are in ccw order.
+type GeoBoundary []GeoCoord
+
+// GeoPolygon is a geofence with 0 or more geofence holes
+type GeoPolygon struct {
+	// GeoFence is the exterior boundary of the polygon
+	GeoFence []GeoCoord
+
+	// Holes is a slice of interior boundary (holes) in the polygon
+	Holes [][]GeoCoord
+}
