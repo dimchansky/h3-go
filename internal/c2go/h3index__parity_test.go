b256e892116d273959a465b1b0d86e780c706f3b
Dmitrij Koniajev <dimchansky@gmail.com>
2025-08-22 11:11:47 +0300

    c2go: add h3Index getters (getResolution/getBaseCellNumber) with parity
    - Go ports matching H3 bit layout masks/offsets
    - Parity tests against C interop; all c2go tests passing

--- internal/c2go/h3index__parity_test.go
+++ internal/c2go/h3index__parity_test.go
@@ -26,3 +26,16 @@ func Test_string_h3_ParityWithC(t *testing.T) {
     }
 }
 
+
+
+func Test_getters_ParityWithC(t *testing.T) {
+    hs := []H3Index{0x8928308280fffff, 0x821c07fffffffff, 0x8a195da49a2ffff}
+    for _, h := range hs {
+        if getResolution(h) != getResolutionC(h) {
+            t.Fatalf("getResolution mismatch for %x", uint64(h))
+        }
+        if getBaseCellNumber(h) != getBaseCellNumberC(h) {
+            t.Fatalf("getBaseCellNumber mismatch for %x", uint64(h))
+        }
+    }
+}
