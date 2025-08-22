53c1c7db95f85e729dcb3a2028ab07769231787c
Dmitrij Koniajev <dimchansky@gmail.com>
2025-08-23 02:23:07 +0300

    internal/c2go: refactor res0CellCount and pentagonCount to use named constants
    Replace magic numbers with proper constants from constants.go:
    - res0CellCount now uses NUM_BASE_CELLS instead of 122
    - pentagonCount now uses NUM_PENTAGONS instead of 12
    
    This improves maintainability and makes the code more self-documenting.

--- internal/c2go/baseCells__res0CellCount.go
+++ internal/c2go/baseCells__res0CellCount.go
@@ -3,5 +3,5 @@ package c2go
 // res0CellCount returns the number of resolution 0 base cells.
 // Ported from H3 C: baseCells.c::res0CellCount
 func res0CellCount() int {
-	return 122 // NUM_BASE_CELLS
+	return NUM_BASE_CELLS
 }
--- internal/c2go/h3index__pentagonCount.go
+++ internal/c2go/h3index__pentagonCount.go
@@ -3,5 +3,5 @@ package c2go
 // pentagonCount returns the number of pentagons at any resolution.
 // Ported from H3 C: h3Index.c::pentagonCount
 func pentagonCount() int {
-	return 12 // NUM_PENTAGONS
+	return NUM_PENTAGONS
 }
